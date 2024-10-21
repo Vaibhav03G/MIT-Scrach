9d1f3a020ca5db185738c87ae09278d8b346280e
Vetal Vaibhav <114693070+Vaibhav03G@users.noreply.github.com>
2024-10-21 11:28:20 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  darkMode: "class",
+  purge: ["./src/**/*.js"],
+  variants: {},
+  theme: {
+    extend: {
+      zIndex: {
+        '1000': '1000',
+      },
+    },
+  },
+  plugins: [],
+};
+
